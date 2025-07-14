a5929bbe9bb1112974bf30634eb6074e80b5b82d
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2025-07-14 12:55:25 +0300

    rajout bootstrap login ok

--- login.php
+++ login.php
@@ -6,19 +6,39 @@ require_once 'includes/fonction.php';
 
 ?>
 
-<form action="loginMethod.php" method="post">
-    <label for="email"> email : </label>
-    <input type="text" name="email" required>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
 
+            <div class="card">
+                <div class="card-header text-center">
+                    <h4>Connexion</h4>
+                </div>
+                <div class="card-body">
+                    <form action="loginMethod.php" method="post">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email :</label>
+                            <input type="text" class="form-control" name="email" required>
+                        </div>
 
-    <br>
+                        <div class="mb-3">
+                            <label for="mdp" class="form-label">Mot de passe :</label>
+                            <input type="password" class="form-control" name="mdp" required>
+                        </div>
 
-    <label for="mdp"> mdp : </label>
-    <input type="text" name="mdp" required>
+                        <div class="d-grid">
+                            <input type="submit" class="btn btn-primary" value="Se connecter">
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer text-center">
+                    <a href="nvProfile.php">Créer un nouveau compte</a>
+                </div>
+            </div>
 
-    <br>   
-    <input type="submit" value="iviyv">
-</form>
+        </div>
+    </div>
+</div>
 
 <br>
 <a href="nvProfile.php">nouveau membre</a>
