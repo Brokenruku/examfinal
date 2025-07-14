b390b3257628365d2b58d9f8fbe9519b580614ae
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2025-07-14 16:06:49 +0300

    aleas1 n2

--- empruntertraitement.php
+++ empruntertraitement.php
@@ -1,22 +1,20 @@
 <?php
-define('APP_ROOT', true);
 require_once 'includes/config.php';
-require_once 'includes/fonction.php';
 
 $id_objet = $_GET['id_objet'];
 $id_membre = $_GET['id_membre'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $jours_emprunt = (int)$_POST['jours_emprunt'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $jours = $_POST['jours'];
     $date_emprunt = date('Y-m-d H:i:s');
-    $date_retour = date('Y-m-d H:i:s', strtotime("+$jours_emprunt days"));
+    $date_retour = date('Y-m-d H:i:s', strtotime("+$jours days"));
     
-    $query = "INSERT INTO emprunt (id_objet, id_membre, date_emprunt, date_retour) VALUES (?, ?, ?, ?)";
-    $stmt = mysqli_prepare($mysqli, $query);
-    mysqli_stmt_bind_param($stmt, "iiss", $id_objet, $id_membre, $date_emprunt, $date_retour);
+    $sql = "INSERT INTO emprunt (id_objet, id_membre, date_emprunt, date_retour) 
+            VALUES ('$id_objet', '$id_membre', '$date_emprunt', '$date_retour')";
     
-    if (mysqli_stmt_execute($stmt)) {
+    if (mysqli_query($mysqli, $sql)) {
         header("Location: listeObjet.php?id_membre=$id_membre");
+        exit();
     }
 }
 ?>
