2ba96837c886463ba650db345d71a03d60f9791e
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2025-07-14 12:21:05 +0300

    final

--- filtration.php
+++ filtration.php
@@ -4,7 +4,7 @@ require_once 'includes/config.php';
 require_once 'includes/headerDedans.php';
 require_once 'includes/fonction.php';
 
-$selectedCategories = isset($_POST['catg']) ? $_POST['catg'] : [];
+$selectedCategories = isset($_POST['catg']) ? $_POST['catg'] : [];  
 $result = filtrationObject($mysqli, $selectedCategories);
 ?>
 
